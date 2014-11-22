0124f78a8e016200d9c112028ea81ac6b74f7872
davidretler <davidretler@gmail.com>
2014-11-21 19:05:47 -0500

    Velocity and Acceleration 	--Speed is now a vector (velocity) 	--Acceleration for both x and y component 	--Todo: tweak acceleration algorithm  Changes to be committed: 	modified:   com/reapersrage/entities/mobs/Player.java

--- com/reapersrage/entities/mobs/Player.java
+++ com/reapersrage/entities/mobs/Player.java
@@ -25,7 +25,11 @@ public class Player {
 	private int width;
 	private int height;
         //Pixels for the player to move every update
-        private int speed;
+        private int[] speed= new int[2];
+        private final int START_SPEED = 5;
+        //How long the player has been moving in each cardinal direction
+        //UP/DOWN LEFT/RIGHT
+        private int[] acceleration = new int[2];
 
 	public Player(int x, int y, int width, int height) {
 		dir = 0;
@@ -37,7 +41,7 @@ public class Player {
 		this.width = width;
 		this.height = height;
                 //Defult to a speed of 5
-                this.speed = 5;
+                this.speed = new int[]{5, 5};
 		try {
 			OImage = ImageIO
 					.read(GameTile.class
@@ -57,27 +61,56 @@ public class Player {
         //Array: {up, down, left, right}
         //Note to self: THE TOP RIGHT CORNER IS 0,0
         public void update(boolean[] dirs){
-            if(dirs[0] && !collision(x, y - speed)[1]){
+            incrementSpeed();
+            int xNew = x;
+            int yNew = y;
+            if(dirs[0] && !collision(x, y - speed[1])[1]){
                 //Up
-                y -= speed;
+                yNew = yNew - speed[1];
             }
-            if(dirs[1] && !collision(x, y + speed)[1]){
+            if(dirs[1] && !collision(x, y + speed[1])[1]){
                 //Down
-                y += speed;
+                yNew = yNew + speed[1];
             }
-            if(dirs[2] && !collision(x - speed, y)[0]){
+            if(dirs[2] && !collision(x - speed[0], y)[0]){
                 //Left
-                x -= speed;
+                xNew = xNew - speed[0];
             }
-            if(dirs[3] && !collision(x + speed, y)[0]){
+            if(dirs[3] && !collision(x + speed[0], y)[0]){
                 //Right
-                x += speed;
+                xNew = xNew + speed[0];
+            }
+            if(xNew == x){
+                //If we're still then acceleration is 0
+                acceleration[0] = 0;
+            } else{
+                //Acceleration goes up by 1 if velocity is positive
+                //Otherwise goes down by one
+                acceleration[0] += xNew > x ? 1 : -1;
+            }
+            if(yNew == y){
+                acceleration[1] = 0;
+            } else{
+                acceleration[1] += yNew > y ? 1 : -1;
             }
             
+            x = xNew;
+            y = yNew;
         }
-                
-        public void setSpeed(int newSpeed){
-            this.speed = newSpeed;
+        
+        //Increment both the x and y speeds based on acceleration
+        public void incrementSpeed(){
+            if(acceleration[0] == 0){
+                speed[0] = START_SPEED;
+            } else{
+                speed[0] += 1;
+            }
+            if(acceleration[1] == 0){
+                speed[1] = START_SPEED;
+            } else{
+                speed[1] += 1;
+            }
+            
         }
 
         //Checks for a collision in both x and y and return an array of booleans indicating such
